b57c4025109c4bd339f8d52466d3d97b7ec38cd0
Jimmy Romero Sejas <JimmyRomero@Fundacion-jala.local>
2016-09-06 14:16:01 -0400

    Fixed the observations

--- src/main/java/EANValidator.java
+++ src/main/java/EANValidator.java
@@ -5,14 +5,14 @@ public class EANValidator {
 
     public static boolean validate(final String eanCode) {
         int sum = 0;
-        int checkSum = Integer.parseInt(eanCode.substring(12));
+        int lastDigit = Integer.parseInt(eanCode.substring(eanCode.length() - 1));
 
         for (int i = 1; i < eanCode.length(); i++) {
             int currentIntFromString = Integer.parseInt(eanCode.substring(i - 1, i));
             sum += (i % 2 == 0) ? 3 * currentIntFromString : currentIntFromString;
         }
 
-        boolean sumDivisibleByTen = sum % 10 == 0;
-        return (sumDivisibleByTen && checkSum == 0) || 10 - (sum % 10) == checkSum;
+        sum = sum % 10 == 0 ? 0 : 10 - (sum % 10);
+        return sum == lastDigit;
     }
 }
--- src/test/java/EANValidatorTest.java
+++ src/test/java/EANValidatorTest.java
@@ -8,33 +8,25 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class EANValidatorTest {
-    EANValidator number;
+    private EANValidator number;
 
     @Before
     public void setUp(){
-        number = new EANValidator("9783815820865");
+        number = new EANValidator();
     }
 
     @Test
-
     public void test_01() {
-
         assertTrue(number.validate("9783815820865"));
-
     }
 
     @Test
     public void test_02() {
-
         assertFalse(number.validate("9783815820864"));
-
     }
 
     @Test
     public void test_03() {
-
         assertTrue(number.validate("9783827317100"));
-
     }
-
 }
